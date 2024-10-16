5757a4813f5db11cc9b21f5fbc5de3a177d33d1d
Sebastián Carabali <secarabali@gmail.com>
2024-10-15 22:55:00 -0500

    Initial commit

---
+++ src/main/java/com/example/bloggin_platform_api/BlogginPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloggin_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogginPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogginPlatformApiApplication.class, args);
+	}
+
+}
